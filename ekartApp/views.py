f3627452324073baf395cd04cef4913e5275dd6d
Darshan PS <darshanpsajeev@gmail.com>
2025-10-14 10:51:47 +0530

    first

--- ekartApp/views.py
+++ ekartApp/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
-
+from ekartApp.models import Product
+from ekartApp.forms import UserRegistartionForm
 # Create your views here.
 
-class HomeView(TemplateView):
-    template_name="index.html"
+class HomeView(View):
+    def get(self,request):
+        products=Product.objects.all()
+        return render(request,'index.html',{'products':products})
+    
+class productView(View):
+    def get(self,request,**kwargs):
+        product=Product.objects.get(id=kwargs.get("id"))
+        return render(request,'product_details.html',{'product':product})
+    
+class userRegisterView(View):
+    def get(self,request):
+        form=UserRegistartionForm()
+        return render(request,"register.html",{"form":form})
     
--- ekartproject/urls.py
+++ ekartproject/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ekartApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomeView.as_view(),name="home_view"),
-]
+
+    path('', views.userRegisterView.as_view(),name="reg"),
+    path('home', views.HomeView.as_view(),name="home_view"),
+    path('pview/<int:id>', views.productView.as_view(),name="pview")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
